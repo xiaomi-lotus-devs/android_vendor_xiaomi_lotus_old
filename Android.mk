1ed64a8088d2294c6ff321251b9b795d268c0fe9
TheMalachite <eliasgheeraert@gmail.com>
2021-04-30 19:10:14 +0530

    lotus: Update AEE Stack from Note_11P
    * From Ulefone/Note_11P/Note_11P:11/RP1A.200720.011/2101131254:user/release-keys
    
    Change-Id: Iba0693ce0abff29a15758256812568e277cc7881
    Signed-off-by: P-Salik <peersalik16@gmail.com>

--- Android.mk
+++ Android.mk
@@ -18,28 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),lotus)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := EmCamera
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/vendor/app/EmCamera/EmCamera.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := MTKThermalManager
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/vendor/app/MTKThermalManager/MTKThermalManager.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
